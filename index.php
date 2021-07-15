9a250a2a5e82315980a6adebcce44882fa16fb65
connellguina <68082396+connellguina@users.noreply.github.com>
2021-07-14 22:10:05 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guina Connell</title>
+</head>
+<body>
+    <main>
+        <ol>
+            <li><a href="/bootstrap page/index.html">bootstrap page</a></li>
+            <li><a href="/materializecss/index.html">materialize page</a></li>
+        </ol>
+    </main>
+</body>
+</html>
 <a href="/materializecss/index.html">Materializecss page</a>
 <a href="/bootstrap page/index.html">bootstrap page</a>
